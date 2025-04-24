a20a610e3ee5528d39012586c0e73b3034735b56
Jessica Estevam <jessicafpereira16@gmail.com>
2025-04-24 20:40:05 -0300

    atualização porta

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -35,5 +36,9 @@ def webhook():
 
     return 'ok'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Porta definida pelo Render ou 5000 localmente
+    app.run(host='0.0.0.0', port=port)
+
+
